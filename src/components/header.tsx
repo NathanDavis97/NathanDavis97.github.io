cadef08df709f57ce268253090bc54082817b12e
NathanDavis97 <davislnathan9@gmail.com>
2022-11-07 16:30:31 -0700

    Update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,6 @@ import styled from "styled-components";
 
 const StyledH1 = styled.h1``;
 
-const Header: React.FC = () => <span> test</span>;
+const Header: React.FC = () => <span> test2</span>;
 
 export default Header;
