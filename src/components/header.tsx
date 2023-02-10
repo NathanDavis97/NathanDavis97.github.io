5488678324782c3269329121e4f7fe31f1ec826f
NathanDavis97 <davislnathan9@gmail.com>
2023-02-10 13:31:47 -0700

    Add h1text and copy props

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 const StyledH1 = styled.h1``;
 
-const Header = ({ height, width }) => {
+const Header = ({ height, width, h1Text, copy }) => {
   return (
-    <div height={height}>
-      <StyledH1></StyledH1>
-      test2
+    <div height={height} width={width}>
+      <StyledH1> {h1Text}</StyledH1>
+      {copy}
     </div>
   );
 };
