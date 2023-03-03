dc67de6f05897dcc8ec6700bd2c3d785deae3a11
NathanDavis97 <davislnathan9@gmail.com>
2023-03-03 11:45:38 -0700

    Remove unneeded button

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -1,32 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Button } from "./Button";
 import "./header.css";
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
     <div className="wrapper">
-      <div>
-        {user ? (
-          <>
-            <span className="welcome">
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
-          </>
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button
-              primary
-              size="small"
-              onClick={onCreateAccount}
-              label="Sign up"
-            />
-          </>
-        )}
-      </div>
+      <div></div>
     </div>
   </header>
 );
