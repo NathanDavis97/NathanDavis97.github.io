02ee81c87cd0176ebb218f4d8e3bdf18c34788b9
NathanDavis97 <davislnathan9@gmail.com>
2023-03-21 12:25:20 -0600

    remove stlyed h1

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,12 +3,7 @@ import styled from "styled-components";
 const StyledH1 = styled.h1``;
 
 const Header = ({ height, width, h1Text, copy }) => {
-  return (
-    <div height={height} width={width}>
-      <StyledH1> {h1Text}</StyledH1>
-      {copy}
-    </div>
-  );
+  return <div>{copy}</div>;
 };
 
 export default Header;
